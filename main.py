028a91a52c6e7fd309b522cb77246971aa18ae88
navouris <navouris@gmail.com>
2023-04-30 14:39:43 +0300

    v1.3

--- main.py
+++ main.py
@@ -8,14 +8,9 @@ def main(page: ft.Page):
         new_task.focus()
         new_task.update()
 
-    def keyboard_handler(event):
-        if event.key == "Enter": add_handler(event)
-
     ## Αρχικοποίηση
     page.title = "Λίστα Καθηκόντων"
     new_task = ft.TextField(hint_text="Να μην ξεχάσω...", color="GRAY", width=250)
     page.add(ft.Row([new_task, ft.ElevatedButton(" + ", on_click=add_handler)]))
-
-    page.on_keyboard_event = keyboard_handler
 
 ft.app(target=main)
